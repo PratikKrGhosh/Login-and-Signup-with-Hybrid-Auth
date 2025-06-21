0c77caec21f1065c47addf77444a1b3e84ac2589
PratikKrGhosh <work.pratikkrghosh@gmail.com>
2025-06-21 12:54:33 +0530

    Solved the server.js port variable bug and renamed the title of login and signup pages

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import app from "./app.js";
 import env from "./config/env.js";
 
-const port = 8000 || env.port;
+const port = parseInt(env.port);
 
 app.listen(port, () => {
   console.log(`Server is running at Port: ${port}`);
